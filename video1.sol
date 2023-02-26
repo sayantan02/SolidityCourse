e866df2ad1da2792ab292c3ee1d8a8d7027a831d
Sayantan Biswas <61279052+sayantan02@users.noreply.github.com>
2023-02-26 17:43:11 +0530

    Video1

---
+++ video1.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract video1 {
+    // State variables
+    // int a;
+
+
+    //  Local variables
+    // function main() {
+    //     int b;
+    // }
+
+    int256 a; // Integer
+    uint256 b; // Unsigned Integer
+    bool id;
+    string ab;
+    address ad;
+    uint[] agh; // [1,2,3,4,5,6]
+
+    // int8 int256
+
+    struct str {
+        uint256 id;
+        string name;
+        bool active;
+    }
+
+
+    mapping(uint256 => string) student;
+
+}
